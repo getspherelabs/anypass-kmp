c35b017606f61c8b1d7ba9cb6ebd53de77116f0e
Behzod Halil <behzoddev@gmail.com>
2023-09-07 19:38:39 +0900

    feat: Add slider tokens

---
+++ shared/src/commonMain/kotlin/io/spherelabs/lockerkmp/components/slider/SliderTokens.kt
@@ -0,0 +1,12 @@
+package io.spherelabs.lockerkmp.components.slider
+
+import androidx.compose.ui.graphics.Color
+
+object SliderTokens {
+    val ThumbColor: Color = Color.Green
+    val DisabledThumbColor: Color = Color.LightGray
+    val TrackColor: Color = Color.Yellow
+    val DisabledTrackColor: Color = Color.LightGray
+    val TickColor: Color = Color.Yellow
+    val DisabledTickColor: Color = Color.DarkGray
+}
