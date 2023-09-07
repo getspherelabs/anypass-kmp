7ac1a2b94696de2c2ac60251db3568df3d717081
Behzod Halil <behzoddev@gmail.com>
2023-09-07 19:38:33 +0900

    feat: Add slider colors

--- shared/src/commonMain/kotlin/io/spherelabs/lockerkmp/components/slider/SliderColors.kt
+++ shared/src/commonMain/kotlin/io/spherelabs/lockerkmp/components/slider/SliderColors.kt
@@ -1,9 +1,6 @@
 package io.spherelabs.lockerkmp.components.slider
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.State
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 import dev.icerock.moko.resources.compose.colorResource
 import io.spherelabs.lockerkmp.MR
@@ -12,8 +9,49 @@ import io.spherelabs.lockerkmp.MR
 @Immutable
 class SliderColors(
     private val thumbColor: Color,
+    private val disabledThumbColor: Color,
     private val trackColor: Color,
-
+    private val disabledTrackColor: Color,
+    private val tickColor: Color,
+    private val disabledTickColor: Color
 ) {
+    @Composable
+    internal fun thumbColor(enabled: Boolean): State<Color> {
+        return rememberUpdatedState(if (enabled) thumbColor else disabledThumbColor)
+    }
 
+    @Composable
+    internal fun trackColor(enabled: Boolean): State<Color> {
+        return rememberUpdatedState(if (enabled) trackColor else disabledTrackColor)
+    }
+
+    @Composable
+    internal fun tickColor(enabled: Boolean): State<Color> {
+        return rememberUpdatedState(if (enabled) tickColor else disabledTickColor)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || other !is SliderColors) return false
+
+        if (thumbColor != other.thumbColor) return false
+        if (trackColor != other.trackColor) return false
+        if (tickColor != other.tickColor) return false
+        if (disabledThumbColor != other.disabledThumbColor) return false
+        if (disabledTrackColor != other.disabledTrackColor) return false
+        if (disabledTickColor != other.disabledTickColor) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = thumbColor.hashCode()
+        result = 31 * result + trackColor.hashCode()
+        result = 31 * result + tickColor.hashCode()
+        result = 31 * result + disabledThumbColor.hashCode()
+        result = 31 * result + disabledTrackColor.hashCode()
+        result = 31 * result + disabledTickColor.hashCode()
+
+        return result
+    }
 }
