ff8893f7796f3addab1a68136e0619540d41b26a
Behzod Halil <behzoddev@gmail.com>
2023-09-07 19:39:15 +0900

    feat: Add tissot progress

---
+++ shared/src/commonMain/kotlin/io/spherelabs/lockerkmp/components/progress/TitanProgress.kt
@@ -0,0 +1,48 @@
+package io.spherelabs.lockerkmp.components.progress
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.icerock.moko.resources.compose.colorResource
+import dev.icerock.moko.resources.compose.fontFamilyResource
+import io.spherelabs.lockerkmp.MR
+import io.spherelabs.lockerkmp.ui.createpassword.purpleDark
+
+@Composable
+internal fun TissotProgress(
+    modifier: Modifier = Modifier
+) {
+    val nbMarker = 120
+
+    val markerActives by animateFloatAsState(
+        targetValue = nbMarker / 30f * 60,
+        animationSpec = tween(500)
+    )
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(105.dp)
+            .background(color = colorResource(MR.colors.dynamic_yellow), shape = CircleShape),
+    ) {
+
+        Text(
+            text = "65",
+            fontFamily = fontFamilyResource(MR.fonts.googlesans.bold),
+            fontSize = 42.sp,
+            color = purpleDark,
+            modifier = Modifier.align(alignment = Alignment.Center)
+        )
+    }
+}
