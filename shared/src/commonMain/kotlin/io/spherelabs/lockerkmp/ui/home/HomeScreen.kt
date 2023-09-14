8719f2241b772931cd71eafe4d11466a416c4b14
Behzod Halil <behzoddev@gmail.com>
2023-09-14 19:26:59 +0900

    feat: Add home route

--- shared/src/commonMain/kotlin/io/spherelabs/lockerkmp/ui/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/io/spherelabs/lockerkmp/ui/home/HomeScreen.kt
@@ -1,51 +1,79 @@
 package io.spherelabs.lockerkmp.ui.home
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Lock
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.Placeable
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.icerock.moko.resources.compose.colorResource
 import dev.icerock.moko.resources.compose.fontFamilyResource
 import dev.icerock.moko.resources.compose.painterResource
+import io.spherelabs.home.homepresentation.*
 import io.spherelabs.lockerkmp.MR
 import io.spherelabs.lockerkmp.components.*
-import io.spherelabs.lockerkmp.components.collapsingToolbar.CollapsingToolbarScaffold
 import io.spherelabs.lockerkmp.components.collapsingToolbar.CollapsingToolbarScaffoldState
-import io.spherelabs.lockerkmp.components.collapsingToolbar.ScrollStrategy
 import io.spherelabs.lockerkmp.components.collapsingToolbar.rememberCollapsingToolbarScaffoldState
+import kotlinx.coroutines.flow.Flow
+import org.koin.compose.rememberKoinInject
 import dev.icerock.moko.resources.compose.painterResource as mokoPainterResource
+
+@Composable
+fun HomeRoute(
+    viewModel: HomeViewModel = rememberKoinInject(),
+    navigateToCreatePassword: () -> Unit
+) {
+    val uiState = viewModel.state.collectAsState()
+
+    HomeScreen(
+        wish = { newWish ->
+            viewModel.wish(newWish)
+        },
+        uiState = uiState,
+        effect = viewModel.effect,
+        navigateToCreatePassword = {
+            navigateToCreatePassword.invoke()
+        }
+    )
+}
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
+    wish: (HomeWish) -> Unit,
+    uiState: State<HomeState>,
+    effect: Flow<HomeEffect>,
     navigateToCreatePassword: () -> Unit
 ) {
     val state = rememberScrollState(0)
     val scaffoldState = rememberScaffoldState()
     val toolbarState: CollapsingToolbarScaffoldState = rememberCollapsingToolbarScaffoldState()
+
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(true) {
+        wish.invoke(HomeWish.GetStartedCategories)
+    }
     Scaffold(
         scaffoldState = scaffoldState,
         backgroundColor = colorResource(MR.colors.lavender),
         topBar = {
             Row(
-                modifier = modifier.fillMaxWidth().padding(start = 24.dp, end = 24.dp),
+                modifier = modifier.fillMaxWidth().padding(start = 24.dp, end = 24.dp, top = 16.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 RoundedImage(
@@ -56,155 +84,39 @@ fun HomeScreen(
                 NewItemButton {
                     navigateToCreatePassword.invoke()
                 }
+
             }
         },
         content = {
             CollapsingLayout(collapsingTop = {
-                Headline(fontSize = 32.sp)
+                Headline()
             }, bodyContent = {
+
                 Column(
-                    modifier = modifier.fillMaxWidth()
+                    modifier = modifier
+                        .fillMaxSize()
                         .background(color = colorResource(MR.colors.lavender))
                         .verticalScroll(state),
                     verticalArrangement = Arrangement.Top
                 ) {
-                    Row(
-                        modifier = modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Tags(
-                            listOf("Categories", "Logins", "Note"),
-                            "Categories",
-                            modifier = modifier
-                        ) {
-
-                        }
-                        Box(
-                            modifier = modifier.padding(top = 12.dp).clip(RoundedCornerShape(20.dp))
-                                .background(color = colorResource(MR.colors.black)).width(56.dp)
-                                .height(42.dp),
-                            contentAlignment = Alignment.Center,
-                        ) {
-                            Icon(
-                                Icons.Default.Add,
-                                contentDescription = null,
-                                tint = colorResource(MR.colors.white)
-                            )
-                        }
+                    if (uiState.value.categories.isNotEmpty()) {
+                        CategoryCard(uiState.value.categories)
                     }
-
-                    Column(modifier = modifier.fillMaxSize()) {
-                        Text(
-                            modifier = modifier.padding(start = 24.dp),
-                            text = "Password Health",
-                            fontSize = 24.sp,
-                            fontFamily = fontFamilyResource(MR.fonts.googlesans.medium),
-                            color = Color.White
-                        )
-
-                        Row(
-                            modifier = modifier.fillMaxWidth().height(250.dp)
-                                .padding(start = 24.dp, end = 24.dp, top = 8.dp, bottom = 8.dp)
-                                .clip(RoundedCornerShape(16.dp))
-                                .background(color = colorResource(MR.colors.dynamic_yellow)),
-                            horizontalArrangement = Arrangement.SpaceAround
-                        ) {
-                            Column(modifier = modifier.wrapContentSize()) {
-                                Box(
-                                    modifier = modifier.padding(start = 12.dp, top = 12.dp)
-                                        .size(32.dp)
-                                        .clip(CircleShape).background(color = Color.Black),
-                                    contentAlignment = Alignment.Center
-                                ) {
-                                    Icon(
-                                        modifier = modifier.size(16.dp),
-                                        imageVector = Icons.Outlined.Lock,
-                                        contentDescription = null,
-                                        tint = Color.White
-                                    )
-                                }
-                                Text(
-                                    modifier = modifier.padding(start = 12.dp, top = 8.dp),
-                                    text = "Total password: 49"
-                                )
-                                Image(
-                                    modifier = modifier.padding(start = 12.dp, top = 8.dp),
-                                    painter = painterResource(MR.images.whale_logo),
-                                    contentDescription = null
-                                )
-                            }
-                            Spacer(modifier = modifier.width(25.dp))
-
-                            Row(
-                                modifier = modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.Center,
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Spacer(modifier = modifier.width(25.dp))
-
-//                                Boxes(
-//                                    modifier = modifier.padding(top = 10.dp)
-//                                        .align(Alignment.CenterVertically)
-//                                ) {
-//                                    Text(
-//                                        text = "56",
-//                                        fontSize = 16.sp,
-//                                        fontFamily = fontFamilyResource(MR.fonts.googlesans.medium)
-//                                    )
-//                                    Box(
-//                                        modifier = modifier.clip(CircleShape)
-//                                            .background(color = Color.White)
-//                                            .size(56.dp),
-//                                        contentAlignment = Alignment.Center,
-//                                    ) {
-//                                        Text(text = "Safe", fontSize = 12.sp, color = Color.Black)
-//                                    }
-//                                    Box(
-//                                        modifier = modifier.clip(CircleShape)
-//                                            .background(color = Color.White)
-//                                            .size(56.dp),
-//                                        contentAlignment = Alignment.Center,
-//                                    ) {
-//                                        Text(text = "Safe", fontSize = 12.sp, color = Color.Black)
-//                                    }
-//                                    Text(text = "56", fontSize = 16.sp)
-//                                    Text(text = "56", fontSize = 16.sp)
+//                    Row(
+//                        modifier = modifier,
+//                        verticalAlignment = Alignment.CenterVertically
+//                    ) {
 //
-//                                    Box(
-//                                        modifier = modifier.clip(CircleShape)
-//                                            .background(color = Color.White)
-//                                            .size(56.dp),
-//                                        contentAlignment = Alignment.Center,
-//                                    ) {
-//                                        Text(text = "Safe", fontSize = 12.sp, color = Color.Black)
-//                                    }
-//
-//                                    Box(
-//                                        modifier = modifier.clip(CircleShape)
-//                                            .background(color = Color.White)
-//                                            .size(56.dp),
-//                                        contentAlignment = Alignment.Center,
-//                                    ) {
-//                                        Text(text = "Safe", fontSize = 12.sp, color = Color.Black)
-//                                    }
-//                                    Text(text = "56", fontSize = 16.sp)
-//
-//                                }
-                            }
+//                        Tags(
+//                            uiState.value.categories,
+//                            categoryUi,
+//                            modifier = modifier
+//                        ) {
+//                            categoryUi = it
+//                        }
+//                    }
 
-                        }
-
-                        Text(
-                            modifier = modifier.padding(start = 24.dp),
-                            text = "Domains",
-                            fontSize = 24.sp,
-                            fontFamily = fontFamilyResource(MR.fonts.googlesans.medium),
-                            color = Color.White
-                        )
-                        DomainCard(modifier)
-
-
-                    }
+//                    CategoryCard(categoryUi, modifier)
 
                 }
 
@@ -384,6 +296,43 @@ fun HomeScreen(
 //            }
 
 
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CategoryCard(
+    categories: List<HomeCategoryUi>,
+    modifier: Modifier = Modifier
+) {
+    val pagerState = rememberPagerState { categories.size }
+
+    println("Categories $categories")
+    val indicator = @Composable { tabPositions: List<TabPosition> ->
+        CustomIndicator(tabPositions, pagerState)
+    }
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        ScrollableTabRow(
+            modifier = Modifier
+                .padding(start = 24.dp, top = 12.dp)
+                .height(30.dp),
+            selectedTabIndex = pagerState.currentPage,
+            indicator = indicator,
+            backgroundColor = Color.Transparent,
+            edgePadding = 0.dp,
+            divider = {
+                Divider(color = Color.Transparent)
+            }
+        ) {
+            categories.forEachIndexed { index, category ->
+                Tag(index, category = category.title, pagerState = pagerState)
+
+            }
+        }
+        ItemPager(modifier = modifier, category = categories, pagerState = pagerState)
+    }
+
+}
+
 @Composable
 fun DomainCard(
     modifier: Modifier = Modifier
@@ -423,7 +372,7 @@ fun DomainCard(
 
 @Composable
 fun Headline(
-    fontSize: TextUnit,
+    fontSize: TextUnit = 32.sp,
     modifier: Modifier = Modifier,
 ) {
     Column(
--- shared/src/commonMain/kotlin/io/spherelabs/lockerkmp/ui/home/navigation/HomeNavigation.kt
+++ shared/src/commonMain/kotlin/io/spherelabs/lockerkmp/ui/home/navigation/HomeNavigation.kt
@@ -1,8 +1,7 @@
 package io.spherelabs.lockerkmp.ui.home.navigation
 
 import io.spherelabs.lockerkmp.navigation.Route
-import io.spherelabs.lockerkmp.ui.confirmpassword.ConfirmPassword
-import io.spherelabs.lockerkmp.ui.home.HomeScreen
+import io.spherelabs.lockerkmp.ui.home.HomeRoute
 import io.spherelabs.navigation.NavHostScope
 import io.spherelabs.navigation.NavigationController
 import io.spherelabs.navigation.composable
@@ -11,7 +10,7 @@ fun NavHostScope<Route>.homeScreen(
     navigateToCreatePassword: () -> Unit
 ) {
     this.composable<Route.Home> {
-        HomeScreen {
+        HomeRoute {
             navigateToCreatePassword.invoke()
         }
     }
