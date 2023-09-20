2d3b0412f9ab6e7b903c1ff217c0ff6372f526bd
Behzod Halil <behzoddev@gmail.com>
2023-09-20 14:53:58 +0900

    feat: Add validation to name #13

---
+++ features/auth/authDomain/src/commonMain/kotlin/io/spherelabs/authdomain/NameValidation.kt
@@ -0,0 +1,12 @@
+package io.spherelabs.authdomain
+
+interface NameValidation {
+    fun execute(name: String): Boolean
+}
+
+class ValidateName : NameValidation {
+
+    override fun execute(name: String): Boolean {
+        return name.length in 3..15 && name.all { it.isLetter() }
+    }
+}
