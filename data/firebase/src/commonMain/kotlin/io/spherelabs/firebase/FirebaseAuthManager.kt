5ae5fd688df3d4e25177ad724a727acd37d22214
Behzod Halil <behzoddev@gmail.com>
2023-09-20 14:49:40 +0900

    feat: Add FirebaseAuthManager to common auth #13

---
+++ data/firebase/src/commonMain/kotlin/io/spherelabs/firebase/FirebaseAuthManager.kt
@@ -0,0 +1,17 @@
+package io.spherelabs.firebase
+
+expect class AuthResult {
+    val user: FirebaseUser?
+}
+
+expect class FirebaseUser {
+    val currentName: String?
+    val email: String?
+    val uid: String?
+}
+
+expect class FirebaseAuthManager {
+    val currentUser: FirebaseUser?
+    suspend fun createEmailAndPassword(email: String, password: String): Result<AuthResult>
+    suspend fun signInWithEmailAndPassword(email: String, password: String): Result<AuthResult>
+}
