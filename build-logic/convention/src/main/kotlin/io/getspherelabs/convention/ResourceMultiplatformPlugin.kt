5afbdb731cfad8229dec1313b766e1576583ac95
behzoddev <behzoddev@gmail.com>
2023-09-28 23:42:58 +0900

    feat: Add  resource plugin (#57)

---
+++ build-logic/convention/src/main/kotlin/io/getspherelabs/convention/ResourceMultiplatformPlugin.kt
@@ -0,0 +1,23 @@
+package io.getspherelabs.convention
+
+import io.getspherelabs.convention.multiplatform.configureResourceMultiplatform
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
+class ResourceMultiplatformPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) = with(target) {
+        with(pluginManager) {
+            apply("com.android.library")
+            apply("org.jetbrains.kotlin.multiplatform")
+            apply("org.jetbrains.compose")
+        }
+        configureCompose()
+        configureSpotless()
+        extensions.configure<KotlinMultiplatformExtension>() {
+            configureResourceMultiplatform(this)
+        }
+    }
+}
