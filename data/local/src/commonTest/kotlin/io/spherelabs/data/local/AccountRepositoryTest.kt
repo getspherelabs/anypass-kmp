8010bb4061992cb87bd2ea0c31e9c2e76e4ad1e9
behzoddev <behzoddev@gmail.com>
2023-10-03 13:52:33 +0900

    test: Add AccountRepository test (#74)

---
+++ data/local/src/commonTest/kotlin/io/spherelabs/data/local/AccountRepositoryTest.kt
@@ -0,0 +1,78 @@
+package io.spherelabs.data.local
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.turbine.test
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import io.spherelabs.accountdomain.repository.AccountRepository
+import io.spherelabs.data.local.db.DefaultPasswordDao
+import io.spherelabs.data.local.db.PasswordDao
+import io.spherelabs.data.local.repository.DefaultAccountRepository
+import io.spherelabs.local.db.AnyPassDatabase
+import io.spherelabs.local.db.Password
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlinx.coroutines.test.runTest
+
+class AccountRepositoryTest {
+
+    private lateinit var sqlDriverFactory: SqlDriver
+    private lateinit var database: AnyPassDatabase
+    private lateinit var dao: PasswordDao
+    private lateinit var repository: AccountRepository
+
+
+    @BeforeTest
+    fun setup() {
+        sqlDriverFactory = TestSqlDriverFactory().createDriver()
+        database = AnyPassDatabase.invoke(sqlDriverFactory)
+        dao = DefaultPasswordDao(database)
+        repository = DefaultAccountRepository(dao)
+    }
+
+    @Test
+    fun `check fetching the total password size`() = runTest {
+        val password1 = Password(
+            id = "1",
+            category_id = "1",
+            email = "test@gmail.com",
+            image = "test_2_image",
+            title = "Behance",
+            notes = "test_notes",
+            password = "12345",
+            username = "Test1",
+            websiteAddress = "Behance.com",
+        )
+        val password2 = Password(
+            id = "2",
+            category_id = "1",
+            email = "test@gmail.com",
+            image = "test_2_image",
+            title = "Linkedin",
+            notes = "test_notes",
+            password = "Strong@2023",
+            username = "Test2",
+            websiteAddress = "Linkedin.com",
+        )
+        val password3 = Password(
+            id = "3",
+            category_id = "1",
+            email = "test@gmail.com",
+            image = "test_2_image",
+            title = "Quora",
+            notes = "test_notes",
+            password = "AbCdEfG1",
+            username = "Test3",
+            websiteAddress = "Quora.com",
+        )
+        dao.insertPassword(password1)
+        dao.insertPassword(password2)
+        dao.insertPassword(password3)
+
+        val result = repository.getPasswordSize()
+
+        result.test {
+            assertThat(awaitItem()).isEqualTo(3)
+        }
+    }
+}
