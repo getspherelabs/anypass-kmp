bb446d92ba9738c62e080a654d7ee4658b15e9c3
behzoddev <behzoddev@gmail.com>
2023-10-03 09:53:22 +0900

    feat: Add FIRApp to koin module (#67)

--- data/authManager/src/iosMain/kotlin/io/spherelabs/firebase/di/Koin.firebase.ios.kt
+++ data/authManager/src/iosMain/kotlin/io/spherelabs/firebase/di/Koin.firebase.ios.kt
@@ -4,9 +4,9 @@ package io.spherelabs.firebase.di
 import cocoapods.FirebaseAuth.FIRAuth
 import io.spherelabs.firebase.FirebaseAuthManager
 import org.koin.dsl.module
-
+import cocoapods.FirebaseCore.FIRApp
 
 actual fun platformModule() = module {
-
+    single { FIRApp.configure() }
     single { FirebaseAuthManager(FIRAuth.auth()) }
 }
