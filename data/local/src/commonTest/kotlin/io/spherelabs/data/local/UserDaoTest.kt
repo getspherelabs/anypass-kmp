b2bbc224e05ae84bf25d1beba8147f56746b04bd
Behzod Halil <behzoddev@gmail.com>
2023-10-05 12:09:16 +0900

    test: Add unit test for user dao (#76)

---
+++ data/local/src/commonTest/kotlin/io/spherelabs/data/local/UserDaoTest.kt
@@ -0,0 +1,43 @@
+package io.spherelabs.data.local
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.turbine.test
+import io.spherelabs.data.local.db.DefaultUserDao
+import io.spherelabs.data.local.db.UserDao
+import io.spherelabs.local.db.AnyPassDatabase
+import io.spherelabs.local.db.User
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlinx.coroutines.test.runTest
+
+class UserDaoTest {
+
+    private lateinit var sqlDriverFactory: SqlDriver
+    private lateinit var database: AnyPassDatabase
+    private lateinit var dao: UserDao
+
+    @BeforeTest
+    fun setup() {
+        sqlDriverFactory = TestSqlDriverFactory().createDriver()
+        database = AnyPassDatabase.invoke(sqlDriverFactory)
+        dao = DefaultUserDao(database)
+    }
+
+    @Test
+    fun `check insert user and get user`() = runTest {
+        val user = User(
+            id = "1",
+            name = "test",
+            email = "test",
+            password = "",
+        )
+
+        dao.insertUser(user)
+        val result = dao.getUser()
+
+        result.test {
+            assertEquals("1", awaitItem().id)
+        }
+    }
+}
