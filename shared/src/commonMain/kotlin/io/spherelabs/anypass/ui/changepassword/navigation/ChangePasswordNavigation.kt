fccab1fcc3bc1dce3e3fc57d8aa6ddff135555b5
behzoddev <behzoddev@gmail.com>
2023-10-12 22:37:54 +0900

    feat: Add change navigation (#73)

---
+++ shared/src/commonMain/kotlin/io/spherelabs/anypass/ui/changepassword/navigation/ChangePasswordNavigation.kt
@@ -0,0 +1,17 @@
+package io.spherelabs.anypass.ui.changepassword.navigation
+
+import io.spherelabs.anypass.navigation.Route
+import io.spherelabs.anypass.ui.changepassword.ChangePasswordRoute
+import io.spherelabs.navigation.NavHostScope
+import io.spherelabs.navigation.NavigationController
+import io.spherelabs.navigation.composable
+
+fun NavigationController<Route>.navigateToChangePassword() {
+    this.navigateTo(Route.ChangePassword)
+}
+
+fun NavHostScope<Route>.changePasswordScreen() {
+    this.composable<Route.ChangePassword> {
+        ChangePasswordRoute()
+    }
+}
