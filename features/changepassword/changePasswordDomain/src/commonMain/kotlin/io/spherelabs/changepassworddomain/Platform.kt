4e99c03edc0b06bb8b7813e5fd81726e147286a3
behzoddev <behzoddev@gmail.com>
2023-10-14 11:34:20 +0900

    feat: Add set new keypassword use case (#73)

--- features/changepassword/changePasswordDomain/src/commonMain/kotlin/io/spherelabs/changepassworddomain/Platform.kt
+++
@@ -1,7 +0,0 @@
-package io.spherelabs.changepassworddomain
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- features/changepassword/changePasswordPresentation/src/commonMain/kotlin/io/spherelabs/changepasswordpresentation/Platform.kt
+++
@@ -1,7 +0,0 @@
-package io.spherelabs.changepasswordpresentation
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
