2e86791b851a01eb96941188d7ffcd2f086ce34c
khurshiddev <umaribnxattob8008@gmail.com>
2023-10-15 16:26:56 +0900

    refactor: add new auth architecture Test (#129)

---
+++ features/auth/authDomain/src/androidUnitTest/kotlin/AddNewAuthDomainArchitectureTest.kt
@@ -0,0 +1,53 @@
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.withNameEndingWith
+import com.lemonappdev.konsist.api.verify.assertTrue
+import org.junit.Test
+
+class AddNewAuthDomainArchitectureTest {
+
+    @Test
+    fun `check classes with 'UseCase' suffix in 'usecase' package`() {
+        Konsist
+            .scopeFromProject()
+            .classes()
+            .withNameEndingWith("UseCase")
+            .assertTrue {
+                it.resideInPackage("..usecase..")
+            }
+    }
+
+    @Test
+    fun `check the interfaces with 'UseCase' suffix in 'usecase' package`() {
+        Konsist
+            .scopeFromProject()
+            .interfaces()
+            .withNameEndingWith("UseCase")
+            .assertTrue {
+                it.resideInPackage("..usecase..")
+            }
+    }
+
+    @Test
+    fun `check interface with 'Repository' suffix in 'repository' package`() {
+        Konsist
+            .scopeFromProject()
+            .interfaces()
+            .withNameEndingWith("Repository")
+            .assertTrue {
+                it.resideInPackage("..repository..")
+            }
+    }
+
+    @Test
+    fun `check use case function name is execute`() {
+        Konsist
+            .scopeFromProject()
+            .classes()
+            .withNameEndingWith("UseCase")
+            .assertTrue {
+                it.hasFunction { function ->
+                    function.name == "execute" && function.hasOverrideModifier
+                }
+            }
+    }
+}
