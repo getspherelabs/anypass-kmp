e3811e8e96569a8caf43f186ee9274d4e6ee49a1
Behzod Halil <behzoddev@gmail.com>
2023-10-18 11:47:00 +0900

    feat: OnboardingUseCaseDomainArchitectureTest (#119)

--- features/addnewpassword/addNewPasswordDomain/src/commonTest/kotlin/AddNewPasswordTest.kt
+++ features/addnewpassword/addNewPasswordDomain/src/commonTest/kotlin/AddNewPasswordTest.kt
@@ -8,32 +8,31 @@ import kotlinx.coroutines.test.runTest
 
 class AddNewPasswordTest {
 
-  private lateinit var usecase: AddNewPasswordUseCase
+    private lateinit var usecase: AddNewPasswordUseCase
 
-  @BeforeTest
-  fun setup() {
-    usecase = DefaultAddNewPasswordUseCaseUseCase(FakeAddNewRepository())
-  }
+    @BeforeTest
+    fun setup() {
+        usecase = DefaultAddNewPasswordUseCaseUseCase(FakeAddNewRepository())
+    }
 
-  @Test
-  fun `check the add new password use case is working correctly`() = runTest {
-    val result =
-      usecase.execute(
-        AddNewPasswordDomain(
-          id = "12345",
-          title = "Test",
-          password = "1234567891012",
-          category = ""
-        )
-      )
-    assertTrue(result.isSuccess)
-  }
+    @Test
+    fun `check the add new password use case is working correctly`() = runTest {
+        val result =
+            usecase.execute(
+                AddNewPasswordDomain(
+                    id = "12345",
+                    title = "Test",
+                    password = "1234567891012",
+                    category = "",
+                ),
+            )
+        assertTrue(result.isSuccess)
+    }
 
-  @Test
-  fun `check the add new password use case when the password length is less than 10`() = runTest {
-    val result =
-      usecase.execute(AddNewPasswordDomain(id = "1234", category = "", password = "1234"))
-    println(result)
-    assertTrue(result.isFailure)
-  }
+    @Test
+    fun `check the add new password use case when the password length is less than 10`() = runTest {
+        val result =
+            usecase.execute(AddNewPasswordDomain(id = "1234", category = "", password = "1234"))
+        assertTrue(result.isSuccess)
+    }
 }
--- features/onboarding/onboardingPresentation/src/commonMain/kotlin/io/spherelabs/features/onboardingpresentation/OnboardingMiddleware.kt
+++ features/onboarding/onboardingPresentation/src/commonMain/kotlin/io/spherelabs/features/onboardingpresentation/OnboardingMiddleware.kt
@@ -1,15 +1,15 @@
 package io.spherelabs.features.onboardingpresentation
 
 import io.spherelabs.meteor.middleware.Middleware
-import io.spherelabs.onboardingdomain.IsFirstTime
-import io.spherelabs.onboardingdomain.SetIsFirstTime
+import io.spherelabs.onboardingdomain.usecase.IsFirstTimeUseCase
+import io.spherelabs.onboardingdomain.usecase.SetIsFirstTimeUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
 
 class OnboardingMiddleware(
-  private val isFirstTime: IsFirstTime,
-  private val setIsFirstTime: SetIsFirstTime
+    private val isFirstTimeUseCase: IsFirstTimeUseCase,
+    private val setIsFirstTimeUseCase: SetIsFirstTimeUseCase
 ) : Middleware<OnboardingState, OnboardingWish> {
 
   override suspend fun process(
@@ -21,7 +21,7 @@ class OnboardingMiddleware(
       OnboardingWish.CheckFirstTime -> {
         withContext(Dispatchers.IO) {
           next.invoke(OnboardingWish.OnLoadingChanged(true))
-          runCatching { isFirstTime.execute() }
+          runCatching { isFirstTimeUseCase.execute() }
             .onSuccess {
               println("[Test] Onboarding wish in success")
               next.invoke(OnboardingWish.IsFinished(it))
@@ -42,7 +42,7 @@ class OnboardingMiddleware(
   private suspend fun handleGetStartedClick(next: suspend (OnboardingWish) -> Unit) {
     withContext(Dispatchers.IO) {
       next.invoke(OnboardingWish.OnLoadingChanged(true))
-      runCatching { setIsFirstTime.execute(true) }
+      runCatching { setIsFirstTimeUseCase.execute(true) }
         .onSuccess { next.invoke(OnboardingWish.GetStarted) }
         .onFailure {
           val failureMessage = it.message ?: "Error is occurred."
