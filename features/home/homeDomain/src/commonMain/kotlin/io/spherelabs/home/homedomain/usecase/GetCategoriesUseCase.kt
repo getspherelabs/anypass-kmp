e5ff7060b710976d8168da0f26fe824301ddb95a
Behzod Halil <behzoddev@gmail.com>
2023-10-18 11:48:27 +0900

    feat: Add KeyPasswordUseCaseDomainArchitectureTest (#119)

--- features/home/homeDomain/src/commonMain/kotlin/io/spherelabs/home/homedomain/usecase/GetCategoriesUseCase.kt
+++ features/home/homeDomain/src/commonMain/kotlin/io/spherelabs/home/homedomain/usecase/GetCategoriesUseCase.kt
@@ -1,14 +1,14 @@
-package io.spherelabs.home.homedomain
+package io.spherelabs.home.homedomain.usecase
 
 import io.spherelabs.home.homedomain.model.HomeCategoryDomain
 import io.spherelabs.home.homedomain.repository.HomeRepository
 import kotlinx.coroutines.flow.Flow
 
-interface GetCategories {
+interface GetCategoriesUseCase {
   fun execute(): Flow<List<HomeCategoryDomain>>
 }
 
-class DefaultGetCategories(private val repository: HomeRepository) : GetCategories {
+class DefaultGetCategoriesUseCase(private val repository: HomeRepository) : GetCategoriesUseCase {
 
   override fun execute(): Flow<List<HomeCategoryDomain>> {
     return repository.getCategories()
