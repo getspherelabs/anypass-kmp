a1bb7d723f610ec1004ddd53cdb309c7984a6372
behzoddev <behzoddev@gmail.com>
2023-10-22 11:27:09 +0900

    test: Add test for otp dao (#115)

--- data/local/src/commonMain/kotlin/io/spherelabs/data/local/repository/DefaultHomeRepository.kt
+++ data/local/src/commonMain/kotlin/io/spherelabs/data/local/repository/DefaultHomeRepository.kt
@@ -2,13 +2,12 @@ package io.spherelabs.data.local.repository
 
 import io.spherelabs.data.local.db.CategoryDao
 import io.spherelabs.data.local.db.PasswordDao
-import io.spherelabs.data.local.db.UserDao
 import io.spherelabs.data.local.mapper.asDomain
 import io.spherelabs.data.local.mapper.asHomeDomain
 import io.spherelabs.home.homedomain.model.HomeCategoryDomain
 import io.spherelabs.home.homedomain.model.HomePasswordDomain
 import io.spherelabs.home.homedomain.repository.HomeRepository
-import io.spherelabs.local.db.Password
+import io.spherelabs.local.db.PasswordEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -23,6 +22,6 @@ class DefaultHomeRepository(
     }
 
     override fun getPasswordsByCategory(id: String): Flow<List<HomePasswordDomain>> {
-        return passwordDao.getPasswordsByCategory(id).map { passwords -> passwords.map { password: Password -> password.asHomeDomain() } }
+        return passwordDao.getPasswordsByCategory(id).map { passwords -> passwords.map { password: PasswordEntity -> password.asHomeDomain() } }
     }
 }
