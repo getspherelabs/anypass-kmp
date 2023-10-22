4b596df2a87fc6c662394f44cfc3e3dd79bf7a0b
behzoddev <behzoddev@gmail.com>
2023-10-22 11:27:46 +0900

    feat: Add CounterDao implementation (#115)

--- data/local/src/commonMain/kotlin/io/spherelabs/data/local/db/otp/dao/CounterDao.kt
+++ data/local/src/commonMain/kotlin/io/spherelabs/data/local/db/otp/dao/CounterDao.kt
@@ -1,5 +1,41 @@
 package io.spherelabs.data.local.db.otp.dao
 
-interface CounterDao {
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import io.spherelabs.local.db.AnyPassDatabase
+import io.spherelabs.local.db.CounterEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.Flow
 
+interface CounterDao {
+    fun getCounters(): Flow<List<CounterEntity>>
+    suspend fun insertCounter(entity: CounterEntity)
+    suspend fun incrementCounter(otpId: String)
+}
+
+class DefaultCounterDao(
+    database: AnyPassDatabase,
+) : CounterDao {
+
+    private val queries = database.counterQueries
+
+    override fun getCounters(): Flow<List<CounterEntity>> {
+       return queries.getCounters().asFlow().mapToList(Dispatchers.IO)
+    }
+
+    override suspend fun insertCounter(entity: CounterEntity) {
+        queries.transaction {
+            queries.insertCounter(
+                otpId = entity.otpId,
+                counter = entity.counter
+            )
+        }
+    }
+
+    override suspend fun incrementCounter(otpId: String) {
+        queries.transaction {
+            queries.incrementCounter(otpId)
+        }
+    }
 }
