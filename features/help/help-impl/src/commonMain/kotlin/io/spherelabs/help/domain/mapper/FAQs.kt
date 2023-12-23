10bd919f8664e259ed3d4cdc928687fd330e1080
Behzod Halil <behzoddev@gmail.com>
2023-12-23 23:04:45 +0900

    feat: Add faqs mapper (#116)

---
+++ features/help/help-impl/src/commonMain/kotlin/io/spherelabs/help/domain/mapper/FAQs.kt
@@ -0,0 +1,16 @@
+package io.spherelabs.help.domain.mapper
+
+import io.spherelabs.helpstore.AskedQuestion
+import io.spherelabs.helpstore.FAQs as StoreFAQs
+import io.spherelabs.model.FAQ
+import io.spherelabs.model.FAQs as DomainFAQs
+
+fun StoreFAQs.asDomain(): DomainFAQs {
+    return this.map { data -> data.asDomain() }
+}
+
+fun AskedQuestion.asDomain(): FAQ {
+    return FAQ(
+        uuid, question, answer,
+    )
+}
