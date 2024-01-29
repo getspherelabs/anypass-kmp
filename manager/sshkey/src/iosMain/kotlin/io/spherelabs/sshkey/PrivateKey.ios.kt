f0a3bb13faef69e205b9ebc42aa039a16dcf877f
Behzod Halil <behzoddev@gmail.com>
2024-01-29 22:45:20 +0900

    Add keystore-ios

--- manager/sshkey/src/iosMain/kotlin/io/spherelabs/sshkey/PrivateKey.ios.kt
+++ manager/sshkey/src/iosMain/kotlin/io/spherelabs/sshkey/PrivateKey.ios.kt
@@ -1,5 +1,9 @@
 package io.spherelabs.sshkey
 
+import kotlinx.cinterop.*
+import platform.CoreFoundation.*
+import platform.Security.*
+
 actual class PrivateKey {
     actual val encoded: ByteArray = byteArrayOf()
 }
